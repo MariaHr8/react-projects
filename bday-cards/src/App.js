e33f7ba3d684116a89c9a317f05f32dd0c4ea599
Maria Hristova <fcmj@abv.bg>
2021-07-31 15:22:28 +0300

    Some basic layout

--- bday-cards/src/App.js
+++ bday-cards/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import data from './data';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <section className="container">
+          <h3> 0 birthdays today </h3>
+          <List />
+          <button
+            onClick={() => console.log('you clicked me')}>Clear All</button>
+        </section>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ bday-cards/src/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        name: "Ana Silver",
+        age: 22,
+        image: 'man.png'
+    },
+    {
+        id: 2,
+        name: "John Brown",
+        age: 25,
+        image: 'man.png'
+    }, 
+    {
+        id: 3,
+        name: "Silvester Miles",
+        age: 20,
+        image: 'man.png'
+    }, 
+    {
+        id: 4,
+        name: "Katty Howling",
+        age: 39,
+        image: 'man.png'
+    }, 
+    {
+        id: 5,
+        name: "Barney Smith",
+        age: 41,
+        image: 'man.png'
+    }
+]
