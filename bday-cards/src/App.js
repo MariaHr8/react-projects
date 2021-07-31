c3292ae03d948b60963b31814a9d178a82441783
Maria Hristova <fcmj@abv.bg>
2021-07-31 17:28:34 +0300

    Finished look +js and css

--- bday-cards/src/App.js
+++ bday-cards/src/App.js
@@ -3,24 +3,21 @@ import './App.css';
 import data from './data';
 import List from './List';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  render() {
-    return (
-      <div className="App" >
-        <section className="container">
-          <h3> 0 birthdays today </h3>
-          <List />
-          <button
-            onClick={() => console.log('you clicked me')}>Clear All</button>
-        </section>
-      </div>
-    );
-  }
+function App() {
+  const [people, setPeople] = useState(data);
 
+  return (
+    <div className="App" >
+      <section className="container">
+        <h3> {people.length} birthdays today </h3>
+        <List people={people}/>
+        <button
+          onClick={() => console.log('you clicked me')}>Clear All</button>
+      </section>
+    </div>
+  );
 }
+
 
 export default App;
--- bday-cards/src/data.js
+++ bday-cards/src/data.js
@@ -1,32 +1,34 @@
+const img = "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg";
+
 export default [
     {
         id: 1,
         name: "Ana Silver",
         age: 22,
-        image: 'man.png'
+        image: img
     },
     {
         id: 2,
         name: "John Brown",
         age: 25,
-        image: 'man.png'
+        image: img 
     }, 
     {
         id: 3,
         name: "Silvester Miles",
         age: 20,
-        image: 'man.png'
+        image: img
     }, 
     {
         id: 4,
         name: "Katty Howling",
         age: 39,
-        image: 'man.png'
+        image: img
     }, 
     {
         id: 5,
         name: "Barney Smith",
         age: 41,
-        image: 'man.png'
+        image: img
     }
-]
+];
